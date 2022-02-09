58ee7328a562f060b2a74caf785353e9948466b1
Walentino <67527927+Walentino@users.noreply.github.com>
2022-02-09 16:46:38 -0500

    Update variable.tf
    Co-authored-by: Vladimir Mukhin <39741635+vladimirmukhin@users.noreply.github.com>

--- variable.tf
+++ variable.tf
@@ -3,6 +3,7 @@ variable "vpc-cidr" {
     description = "VPC CIDR Block"
     type = string
 }
+
 variable  "public-subnet-1-cidr" {
     default = "10.0.0.0/24"
     description = "Public Subnet 1 CIDR Block"
